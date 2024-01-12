9ab886454590cfb8f88738d6e361dac0ed0b1ea8
HetProm <hetprom@gmail.com>
2024-01-12 15:47:36 +0100

    update

--- components/testy.vue
+++ components/testy.vue
@@ -9,29 +9,33 @@
 <script>
 import axios from 'axios';
 
-try {
-  const response = await axios.post(
-    'https://eu-central-1.aws.data.mongodb-api.com/app/data-phkxf/endpoint/data/v1/action/findOne',
-    {
-      collection: 'offerts',
-      database: 'dbOfferts',
-      dataSource: 'bitpraca',
-      projection: {
-        _id: 1,
-        url: 1
+async function fetchData23() {
+  try {
+    const response = await axios.post(
+      'https://eu-central-1.aws.data.mongodb-api.com/app/data-phkxf/endpoint/data/v1/action/findOne',
+      {
+        'collection': 'offerts',
+        'database': 'dbOfferts',
+        'dataSource': 'bitpraca',
+        'projection': {
+          '_id': 1,
+          'url': 1
+        }
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': process.env.APIKEY
+        }
       }
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key': process.env.APIKEY
-      }
+    );
+    console.log(response.data.document);
+    } catch (error) {
+    console.error(error);
     }
-  );
-
-  console.log(response.data.document);
-} catch (error) {
-  console.error(error);
 }
+
+fetchData23();
 
 </script>
