c6c2178e5bb4f274a0586f4d88024896cde527d1
HetProm <hetprom@gmail.com>
2024-01-13 15:14:05 +0100

    test v3

--- components/testy.vue
+++ components/testy.vue
@@ -7,32 +7,21 @@
 </template>
 
 <script>
+import mongoose from 'mongoose';
+ async function nuxtServerInit() {
+    try {
+      await mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = process.env.DATABASE_URL;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Connection error:', error);
+    }
   }
-});
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
+  nuxtServerInit()
 
 </script>
--- server/plugins/dbconnection.js
+++ server/plugins/dbconnection.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
 export default async () => {
-    const config = useRuntimeConfig();
-    try {
-        const DB_OPTIONS = {
-            dbName: config.dbName,
-            user: config.user,
-            pass: config.pass,
-            authSource: config.authSource
-        };
-        await mongoose.connect(config.dburl, DB_OPTIONS);
-        console.log("Connected Successfully");
-    } catch (error) {
-        console.error(`Error connecting ${config.dbName} url: ${config.dburl} to MongoDB:`, error);
-    }
+    console.log('base')
+    // const config = useRuntimeConfig();
+    // try {
+    //     const DB_OPTIONS = {
+    //         dbName: config.dbName,
+    //         user: config.user,
+    //         pass: config.pass,
+    //         authSource: config.authSource
+    //     };
+    //     await mongoose.connect(config.dburl, DB_OPTIONS);
+    //     console.log("Connected Successfully");
+    // } catch (error) {
+    //     console.error(`Error connecting ${config.dbName} url: ${config.dburl} to MongoDB:`, error);
+    // }
 }
