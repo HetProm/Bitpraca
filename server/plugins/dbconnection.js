1c1eeab540099f266701edefe435946b7ed1281c
HetProm <hetprom@gmail.com>
2024-01-14 15:33:47 +0100

    test v12

--- server/plugins/dbconnection.js
+++ server/plugins/dbconnection.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 export default defineNitroPlugin( async (nitroApp) => {
 
-    const config = useRuntimeConfig();
-    try {
-        const DB_OPTIONS = {
-            dbName: config.dbName,
-            user: config.user,
-            pass: config.pass,
-            authSource: config.authSource
-        };
-        await mongoose.connect(config.dburl, DB_OPTIONS);
-        console.log("Connected Succesfully");
-    } catch (error) {
-        console.log(error);
+    // const config = useRuntimeConfig();
+    // try {
+    //     const DB_OPTIONS = {
+    //         dbName: config.dbName,
+    //         user: config.user,
+    //         pass: config.pass,
+    //         authSource: config.authSource
+    //     };
+    //     await mongoose.connect(config.dburl, DB_OPTIONS);
+    //     console.log("Connected Succesfully");
+    // } catch (error) {
+    //     console.log(error);
         
-    }
+    // }
 })
