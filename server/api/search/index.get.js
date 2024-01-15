e49777373f4fd09a5665bb4323a1b9955a51bcdc
HetProm <hetprom@gmail.com>
2024-01-15 21:08:31 +0100

    now

--- server/api/search/index.get.js
+++ server/api/search/index.get.js
@@ -1,40 +1,23 @@
-import axios from 'axios';
+import OffertModel from "~/server/models/Offert";
+
 
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
     const searchTerm = query.searchTerm;
 
-    try {
+
+    try{
         if (searchTerm) {
-            const apiUrl = 'https://eu-central-1.aws.data.mongodb-api.com/app/data-phkxf/endpoint/data/v1/action/find';
-            const response = await axios.post(apiUrl, {
-                collection: 'offerts',
-                database: 'dbOfferts',
-                dataSource: 'bitpraca',
-                projection: {
-                    '_id': 0,
-                    'expectedTitle': 1,
-                    // Dodaj inne pola, których potrzebujesz
-                },
-                filter: {
-                    expectedTitle: { $regex: searchTerm, $options: 'i' },
-                },
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'api-key': process.env.APIKEY,
-                },
-            });
-            const uniqueExpectedTitles = [...new Set(response.data.documents.map(item => item.expectedTitle))];
-            console.log(uniqueExpectedTitles)
-            return {uniqueExpectedTitles}
+            const searchResult = await OffertModel.distinct(
+                'expectedTitle',
+                {expectedTitle: {$regex: searchTerm, $options: 'i'}}    
+                );
+            return searchResult;
         }
+       
+       
     } catch (error) {
         console.error(error);
-        throw createError({
-            statusCode: 400,
-            statusMessage: 'Wystąpił błąd podczas przetwarzania zapytania.',
-        })
-        
+        return "Wystąpił błąd podczas przetwarzania zapytania."
     }
-});
+})
